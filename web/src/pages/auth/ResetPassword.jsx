b556ca4119f5dd6160e7d9e10d745fd5453ec3ee
Horlakz <horlakz@protonmail.com>
2021-12-23 19:31:40 +0100

    removed sample

--- web/src/pages/auth/ResetPassword.jsx
+++ web/src/pages/auth/ResetPassword.jsx
@@ -2,16 +2,10 @@ import AuthLayout from "../../layouts/AuthLayout";
 import InputGroup from "../../components/auth/InputGroup";
 
 export default function ResetPassword() {
-  const reset_password = (e) => {
-    e.preventDefault()
-  }
 
   return (
       <AuthLayout pgtitle="Reset Password">
-        <form onSubmit={reset_password}>
           <InputGroup type="email" contenttitle="Email" />
-          <button type="submit">Reset</button>
-        </form>
       </AuthLayout>
   );
 }
