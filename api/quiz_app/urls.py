9636808bfffa2fc59243482b6979fffbef837813
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2021-12-23 18:29:02 +0100

    adds path for docs using swagger schema

--- api/quiz_app/urls.py
+++ api/quiz_app/urls.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.urls.conf import re_path
 
-urlpatterns = [path("admin/", admin.site.urls), path("", include("accounts.urls"))]
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title="Quiz API Documentation")
+
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    # api docs
+    re_path(r"^docs/", schema_view),
+    path("", include("accounts.urls")),
+]
