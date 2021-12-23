afbcf16ba61dc22bad4d3d0ef7b876a2301373d1
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2021-12-23 17:49:22 +0100

    Update ResetPassword.jsx

--- web/src/pages/auth/ResetPassword.jsx
+++ web/src/pages/auth/ResetPassword.jsx
@@ -2,9 +2,16 @@ import AuthLayout from "../../layouts/AuthLayout";
 import InputGroup from "../../components/auth/InputGroup";
 
 export default function ResetPassword() {
+  const reset_password = (e) => {
+    e.preventDefault()
+  }
+
   return (
       <AuthLayout pgtitle="Reset Password">
-        <InputGroup type="email" contenttitle="Email" />
+        <form onSubmit={reset_password}>
+          <InputGroup type="email" contenttitle="Email" />
+          <button type="submit">Reset</button>
+        </form>
       </AuthLayout>
   );
 }
