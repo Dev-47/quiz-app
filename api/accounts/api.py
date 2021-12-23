9318a8aadafaa20754bcec9abc3b10412eb02431
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2021-12-23 17:16:21 +0100

    adds empty api auth views to api.py and created endpoints in urls.py

---
+++ api/accounts/api.py
@@ -0,0 +1,21 @@
+from rest_framework.generics import GenericAPIView as View
+
+
+class RegisterUserAPI(View):
+    pass
+
+
+class LogoutUserAPI(View):
+    pass
+
+
+class ForgotPasswordUserAPI(View):
+    pass
+
+
+class DashboardUserAPI(View):
+    pass
+
+
+class ProfileUserAPI(View):
+    pass
