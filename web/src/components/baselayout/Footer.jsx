5e0b4f807504341e94eeb6b58fcd0c3eda3710f2
Horlakz <horlakz@protonmail.com>
2021-12-29 06:31:20 +0100

    fixed console error

--- web/src/components/baselayout/Footer.jsx
+++ web/src/components/baselayout/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <footer className="text-black flex justify-between mt-14 place-items-center px-8">
       <a href="https://github.com/dev-47">
         <div className="logo m-10">
-          <spsan className="text-[#e01936] text-2xl">DEV &nbsp;</spsan>
+          <span className="text-[#e01936] text-2xl">DEV &nbsp;</span>
           <span className="text-[#026294] text-2xl">47</span>
         </div>
       </a>
