3bc02b8dc80532ee2b9494017446b1588513dee5
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2021-12-30 22:18:57 +0100

    adds quiz list page

---
+++ web/src/pages/quiz/QuizList.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import QuizItem from "../../components/quiz/QuizItem";
+
+export default function QuizList() {
+  return (
+    <div>
+      <QuizItem />
+      <QuizItem />
+      <QuizItem />
+    </div>
+  );
+}
