b19d80784e333ab4d899a886ff161ee5098aa006
mustapha jay <mustaphajay2@gmail.com>
2021-12-30 22:26:27 +0100

    add files

---
+++ web/src/pages/QuizResult.jsx
@@ -0,0 +1,8 @@
+export default function QuizResult () {
+    return (
+      <div>
+        <h1>hello world</h1>
+        <button>help!!</button>
+      </div>
+    );
+  }
--- web/src/router/index.jsx
+++ web/src/router/index.jsx
@@ -6,6 +6,7 @@ import SignIn from "../pages/auth/SignIn";
 import SignUp from "../pages/auth/SignUp";
 import ResetPassword from "../pages/auth/ResetPassword";
 import ErrorPage from "../pages/error/ErrorPage";
+import QuizResult from "../pages/QuizResult"
 
 export default function Router() {
   return (
@@ -19,6 +20,8 @@ export default function Router() {
       <Route path="/sign-in" element={<SignIn />} />
       <Route path="/sign-up" element={<SignUp />} />
       <Route path="/reset-password" element={<ResetPassword />} />
+      <Route path="/quiz-result" element={<QuizResult />} />
     </Routes>
+    
   );
 }
