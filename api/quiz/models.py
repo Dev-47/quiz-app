71f96d2a5a4c6a26c0eeb0578d2d05e5e2acf4ad
Mohammed Al Ameen <ameenmohammed2311@gmail.com>
2021-12-31 13:12:18 +0100

    added quiz model utils to add questions and mark user submissions

--- api/quiz/models.py
+++ api/quiz/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.utils import timezone, tree
+from django.utils import timezone
 
 from quiz_app.base import BaseModel
 from accounts.models import User
@@ -13,19 +13,41 @@ class Quiz(BaseModel):
     enable_time = models.BooleanField(default=False)
     enable_negative_marking = models.BooleanField(default=False)
 
+    def __str__(self):
+        return self.title
+
     def add_question(self, question, options, answer):
-        __answer = Option.objects.create(text=answer)
+        # filter answer from options or create a new one
+        __answer_qs = Option.objects.filter(text=answer)
+        __answer = __answer_qs.first()
+
+        if not __answer_qs.exists():
+            __answer = Option.objects.create(text=answer)
+            __answer.save()
+
         __question = Question.objects.create(
-            quiz_id=self.id, question=question, answer=__answer
+            quiz=self, question=question, answer=__answer
         )
-        __question.add_options(options)
-        __question.set_answer(answer)
         __question.save()
+        __question.add_options(*options)
+
+        return __question
+
+    def mark_quiz_user(self, user, questions):
+        return {"score": "40%", "summary": "4 out 10"}
+
+    @property
+    def questions(self):
+        questions = Question.objects.filter(quiz_id=self.id)
+        return questions
 
 
 class Option(BaseModel):
 
-    text = models.TextField(blank=False, unique=True)
+    text = models.TextField(blank=False)
+
+    def __str__(self):
+        return self.text
 
 
 class Question(BaseModel):
@@ -35,20 +57,15 @@ class Question(BaseModel):
     options = models.ManyToManyField(Option, related_name="option")
     answer = models.ForeignKey(Option, related_name="answer", on_delete=models.CASCADE)
 
+    def __str__(self):
+        return self.question
+
     def add_options(self, *texts):
-        for __text in texts:
-            option, created = Option.objects.get_or_create(text=__text)
+        for text in texts:
+            option_qs = Option.objects.filter(text=text)
+            option = option_qs.first()
+
+            if not option_qs.exists():
+                option = Option.objects.create(text=text)
+
             self.options.add(option)
-
-            if not created:
-                option.save()
-        self.save()
-
-    def set_answer(self, text):
-        option, created = Option.objects.get_or_create(text=text)
-
-        if not created:
-            option.save()
-
-        self.answer = option
-        self.save()
